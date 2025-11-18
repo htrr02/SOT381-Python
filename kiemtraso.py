f0741c420634d684b4d113bb296688bf7be8a78a
Trần Huyền Trang <trang.th.67khmt@ntu.edu.vn>
2025-11-18 08:35:06 +0700

    Create kiemtraso.py

---
+++ kiemtraso.py
@@ -0,0 +1,8 @@
+number = int(input("Nhập vào một số:"))
+
+if number > 0:
+    print(number, "là số dương")
+elif number < 0:
+    print(number, "là số âm")
+else:
+    print(number,"là số không âm cũng không dương")
