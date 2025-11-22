c1b9fe98a5b8a5c9acca501ecec47c53b0c19d4f
Trần Huyền Trang <trang.th.67khmt@ntu.edu.vn>
2025-11-22 16:46:29 +0700

    Create xep_loai_hoc_luc.py

---
+++ xep_loai_hoc_luc.py
@@ -0,0 +1,10 @@
+dtb = int(input("Nhập điểm trung bình của bạn:"))
+
+if dtb >= 8.0 :
+     print("Học lực giỏi")
+elif dtb >= 6.5 :
+     print("Học lực khá")
+elif dtb >= 5.0 :
+     print("Học lực trung bình")
+else:
+    print("Học lực yếu")
