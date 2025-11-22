0865187d628cd37352a08d225efd3c523bfd8834
Trần Huyền Trang <trang.th.67khmt@ntu.edu.vn>
2025-11-22 16:46:22 +0700

    Create kiem_tra_ngay.py

---
+++ kiem_tra_ngay.py
@@ -0,0 +1,16 @@
+ngay = input(" Nhập một số nguyên từ 2 -> 8:")
+
+if ngay == 2:
+    print("Thứ 2")
+elif ngay == 3:
+    print("Thứ ba")
+elif ngay == 4:
+    print("Thứ tư")
+elif ngay == 5:
+    print ("Thứ năm")
+elif ngay == 6:
+    print("Thứ sáu")
+elif ngay == 7:
+    print("Thứ bảy")
+else:
+    print("Chủ nhật")
