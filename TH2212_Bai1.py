9ffc734855e0efe887749aeaa1d0bbb0b9508a53
Trần Huyền Trang <trang.th.67khmt@ntu.edu.vn>
2025-12-22 14:17:04 +0700

    Create TH2212_Bai1.py

---
+++ TH2212_Bai1.py
@@ -0,0 +1,13 @@
+w = float(input(" Nhập độ dài cạnh w của hình chữ nhật:"))
+h = float(input("Nhập độ dài cạnh h của hình chữ nhật: "))
+
+if 0.0<=w and h<=100.0 :
+    chu_vi = (w+h)*2
+    dien_tich = w*h
+    print(f"Chu vi của hình chữ nhật là: {chu_vi:.2f}")
+    print(f"Diện tích của hình chữ nhật là:{dien_tich:.2f}")
+
+else:
+     print("Vui lòng nhập iá trị hợp lệ")
+
+
